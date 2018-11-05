9aa853373fa72d3a70d58b7f4b842b9eb40cf457
wkwzzc <31513387+wkwzzc@users.noreply.github.com>
2018-11-05 18:45:54 +0800

    Update DbscanSettings.scala
    添加经纬度距离的计算方法

--- src/src/main/scala/org/alitouka/spark/dbscan/DbscanSettings.scala
+++ src/src/main/scala/org/alitouka/spark/dbscan/DbscanSettings.scala
@@ -78,6 +78,9 @@ class DbscanSettings extends Serializable {
     _numPoints = np
     this
   }
+ 
+ 
+ 
 }
 
 /** Provides default values for parameters of the DBSCAN algorithm
@@ -91,4 +94,43 @@ object DbscanSettings {
   def getDefaultEpsilon: Double = { 1e-4 }
 
   def getDefaultNumberOfPoints: Int = { 3 }
+ 
+ def getLonLatDistance:DistanceMeasure = {new LonLatDistance()}
+}
+
+
+class LonLatDistance extends DistanceMeasure {
+  /**
+    * the array's dim = 2
+    * Arrry(0) = Lon ,Array(1) = Lat
+    */
+  override def compute(a: Array[Double], b: Array[Double]): Double = {
+
+    val lon1 = a(0)
+    val lon2 = b(0)
+    val lat1 = a(1)
+    val lat2 = b(1)
+
+    require(math.abs(lon1) <= 180)
+    require(math.abs(lon2) <= 180)
+    require(math.abs(lat1) <= 90)
+    require(math.abs(lat2) <= 90)
+
+    import Math._
+    // Setup the inputs to the formula
+    val R = 6371009d // average radius of the earth in metres
+    val dLat = toRadians(lat2 - lat1)
+    val dLng = toRadians(lon2 - lon1)
+    val latA = toRadians(lat1)
+    val latB = toRadians(lat2)
+    // The actual haversine formula. a and c are well known value names in the formula.
+    val haversinea = sin(dLat / 2) * sin(dLat / 2) +
+      sin(dLng / 2) * sin(dLng / 2) * cos(latA) * cos(latB)
+    val haversinec = 2 * atan2(sqrt(haversinea), sqrt(1 - haversinea))
+
+    //The unit is m
+    R * haversinec
+
+  }
+
 }
